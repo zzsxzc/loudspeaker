e55ea84655e803b3e7105b707da9a0ff43250f0d
zhengzhaosheng <1350111595@qq.com>
2020-11-23 22:26:33 +0800

    Initial commit

---
+++ app/src/main/java/com/zzsxzc/loudspeaker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zzsxzc.loudspeaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+*@Author zheng zs
+*@Date 22:22 2020/11/23
+*@Description 
+**/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
